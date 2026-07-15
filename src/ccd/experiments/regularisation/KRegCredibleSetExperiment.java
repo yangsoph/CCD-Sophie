83f8fb0d13d9ad41e25722d24d5cf8cd3a7b83fb
Sophie Yang <zyangsophie@gmail.com>
2026-07-15 17:27:53 +1200

    Update KRegCredibleSetExperiment.java

--- src/ccd/experiments/regularisation/KRegCredibleSetExperiment.java
+++ src/ccd/experiments/regularisation/KRegCredibleSetExperiment.java
@@ -20,7 +20,7 @@ public class KRegCredibleSetExperiment {
     public static String ccdType; // ccd0, ccd1, kreg
     public static int startRep; // the starting index of reps, used in the case when nesi killed job half way
     public static int endRep; // the ending index of reps, used in the case when nesi killed job half way
-    public static int precision = 50; // the number of thresholds used to determine credible levels in credible set.
+    public static int precision = 100; // the number of thresholds used to determine credible levels in credible set.
 
     public static void main(String[] args) throws IOException {
 
@@ -38,14 +38,14 @@ public class KRegCredibleSetExperiment {
             endRep = 1;
         }
 
-        // output file
-        // String outputPathName = "/nesi/nobackup/uoa04397/sophie/smoothing/KReg/credSet_fixedAlpha_output/credibleSet_" + dataName + "_sub" + subsampleSize + "_" + ccdType + "_" + startRep + ".csv";
-        String outputPathName = "/Users/zyan598/Desktop/local_test/credibleSetCheck.csv";
+        // output file for credible set fraction
+        String outputPathName = "/nesi/nobackup/uoa04397/sophie/smoothing/KReg/credSet_fixedAlpha_output/credibleSet_" + dataName + "_sub" + subsampleSize + "_" + ccdType + "_" + startRep + ".csv";
+        // String outputPathName = "/Users/zyan598/Desktop/local_test/credibleSetCheck.csv";
         File outputFile = new File(outputPathName);
         FileWriter fileWriter = new FileWriter(outputFile);
         PrintWriter writer = new PrintWriter(fileWriter);
 
-        // header
+        // header for credible set fraction
         String separator = ",";
         StringBuilder sb = new StringBuilder();
         sb.append("rep").append(separator);
@@ -57,22 +57,36 @@ public class KRegCredibleSetExperiment {
         writer.println(sb.toString());
         writer.flush();
 
+        PrintWriter writerOptiMu = null;
+        if (ccdType.equals("kreg")) {
+            // output file for optimal mu
+            String outputPathNameOptiMu = "/nesi/nobackup/uoa04397/sophie/smoothing/KReg/optimal_mu/optimal_mu_" + dataName + "_sub" + subsampleSize + "_" + ccdType + "_" + startRep + ".csv";
+            File outputFileOptiMu = new File(outputPathNameOptiMu);
+            FileWriter fileWriterOptiMu = new FileWriter(outputFileOptiMu);
+            writerOptiMu = new PrintWriter(fileWriterOptiMu);
+            // header for optimal mu
+            sb = new StringBuilder();
+            sb.append("rep").append(separator);
+            sb.append("optimisedlMu");
+            writerOptiMu.println(sb.toString());
+            writerOptiMu.flush();
+        }
+
         for (int rep = startRep; rep <= endRep; rep++) {
 
-            // String trainingTreesDir = "/nesi/nobackup/uoa04397/sophie/wcss_full/" + dataName + "/rep" + rep + "/run1/"
-            //         + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
-            // String testingTreesDir = "/nesi/nobackup/uoa04397/sophie/wcss_full/" + dataName + "/rep" + rep + "/run2/"
-            //         + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
+            String trainingTreesDir = "/nesi/nobackup/uoa04397/sophie/wcss_full/" + dataName + "/rep" + rep + "/run1/"
+                    + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
+            String testingTreesDir = "/nesi/nobackup/uoa04397/sophie/wcss_full/" + dataName + "/rep" + rep + "/run2/"
+                    + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
 
-            String trainingTreesDir = "/Volumes/DYNABOOK/wcss_full/" + dataName + "/rep" + rep + "/run1/"
-                    + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
-            String testingTreesDir = "/Volumes/DYNABOOK/wcss_full/" + dataName + "/rep" + rep + "/run2/"
-                    + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
+            // String trainingTreesDir = "/Volumes/DYNABOOK/wcss_full/" + dataName + "/rep" + rep + "/run1/"
+            //         + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
+            // String testingTreesDir = "/Volumes/DYNABOOK/wcss_full/" + dataName + "/rep" + rep + "/run2/"
+            //         + dataName.substring(0, 4).toLowerCase() + "-n" + dataName.substring(4, dataName.length()) + "-" + rep + ".trees";
 
             File trainingTreesFile = new File(trainingTreesDir);
             File testingTreesFile = new File(testingTreesDir);
 
-            // TreeAnnotator.MemoryFriendlyTreeSet mcmcTreesHalf1 = new TreeAnnotator().new MemoryFriendlyTreeSet(treeSetDirHalf1, 0);
             List<Tree> trainingTrees = LoadOrStoreTrees.loadTrees(trainingTreesFile, 0, subsampleSize);
             List<Tree> testingTrees = LoadOrStoreTrees.loadTrees(testingTreesFile, 0, subsampleSize);
 
@@ -86,6 +100,12 @@ public class KRegCredibleSetExperiment {
                 probBasedCredSetComputer = new ProbabilityBasedCredibleSetComputer(ccd1);
             } else {
                 KRegCCD kreg = KRegCCD.withOptimisedMu(trainingTrees);
+                double optiMu = kreg.getMu();
+                sb = new StringBuilder();
+                sb.append(rep).append(separator);
+                sb.append(optiMu);
+                writerOptiMu.println(sb.toString());
+                writerOptiMu.flush();
                 probBasedCredSetComputer = new ProbabilityBasedCredibleSetComputer(kreg);
             }
             int[] countsPerCredibleLevel = getCountsPerCredibleLevel(probBasedCredSetComputer, testingTrees);
@@ -111,8 +131,8 @@ public class KRegCredibleSetExperiment {
         for (Tree tree : treeList) {
             double credibleLevel = probBasedCredSetComputer.getCredibleLevel(tree);
             if (credibleLevel < 0) continue; // skip trees with 0 probability: getCredibleLevel returns -1 if prob == 0
-            // int index = (int) Math.floor(credibleLevel * precision) - 1;
-            int index = (int) Math.ceil(credibleLevel * precision) - 1;
+            int index = (int) Math.floor(credibleLevel * precision) - 1;
+            // int index = (int) Math.ceil(credibleLevel * precision) - 1;
             for (int i = index; i < precision; i++) {
                 countsPerCredibleLevel[i]++;
             }
